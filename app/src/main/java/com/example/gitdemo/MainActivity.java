738097a7ddc1d715dde0ab4f69aa8c41e4ce8884
bhargavhada <bhargav.hada22@gmail.com>
2020-03-19 18:46:53 +0530

    test msg

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -13,10 +13,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
 }
